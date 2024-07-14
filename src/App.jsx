cb2749f6b2a35a52e50fb6be758e70e60b7942fa
nobleknightt <60701544+nobleknightt@users.noreply.github.com>
2024-07-14 19:20:35 +0530

    added github pages deployment, fixed urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import icon from "./assets/icon.png"
 
 function App() {
 
@@ -10,14 +11,14 @@ function App() {
       title: "Contests Web App",
       description: "A Progressive Web App which shows upcoming and ongoing contests from AtCoder, CodeChef, Codeforces, GeeksforGeeks and LeetCode",
       techstack: ["React", "Tailwind CSS", "Python", "Web Scraping"],
-      url: "https://er-knight.github.io/contests",
-      github: "https://github.com/er-knight/contests"
+      url: "https://nobleknightt.github.io/contests",
+      github: "https://github.com/nobleknightt/contests"
     },
     {
       title: "Web Search for ChatGPT",
       description: "Browser extension which updates ChatGPT on latest events using Google, based on RAG (Retrieval Augmented Generation)",
       techstack: ["JavaScript", "SerpAPI"],
-      github: "https://github.com/er-knight/chatgpt-web-access"
+      github: "https://github.com/nobleknightt/web-search-for-chatgpt"
     }
   ]
 
@@ -39,9 +40,11 @@ function App() {
 
 
   return (
-    <div className="flex flex-col font-['Work_Sans'] p-2 h-screen">
+    <div className="flex flex-col font-['Bricolage_Grotesque'] p-2 h-screen">
       <div className="flex justify-between mx-2 px-2 md:px-4 pb-2 border-b">
-        <div className="text-4xl tracking-[-3.6px]">ad<span className="font-bold">.</span></div>
+        <div className="flex items-center justify-center hover:animate-spin">
+          <img src={icon} width={23} height={24}></img>
+        </div>
         <div className="hidden sm:flex gap-8 justify-center items-center text-xl tracking-tighter">
           <div className="underline decoration-1 decoration-gray-200 hover:decoration-gray-950 hover:cursor-pointer" onClick={() => {
             document.getElementById("about").scrollIntoView({ behavior: "smooth" })
@@ -49,7 +52,8 @@ function App() {
           <div className="underline decoration-1 decoration-gray-200 hover:decoration-gray-950 hover:cursor-pointer" onClick={() => {
             document.getElementById("projects").scrollIntoView({ behavior: "smooth" })
           }}>Projects</div>
-          <div className="underline decoration-1 decoration-gray-200 hover:decoration-gray-950">Blog</div>
+          <div className="underline decoration-1 decoration-gray-200 hover:decoration-gray-950">
+            <a href="https://nobleknight.hashnode.dev" target="_blank">Blog</a></div>
         </div>
         <div className="flex sm:hidden justify-center items-center text-xl tracking-tighter relative">
           <div className="underline decoration-1 decoration-gray-200 hover:decoration-gray-950 border px-2 hover:border-gray-950" onClick={() => setShowMenu(!showMenu)}>Menu</div>
@@ -67,7 +71,7 @@ function App() {
       <div className="grow overflow-scroll">
         <div className="h-full flex flex-col items-center justify-center gap-6 md:gap-8 border-b">
           <div className="text-6xl md:text-7xl lg:text-8xl xl:text-9xl tracking-tighter text-center">Ajay Dandge</div>
-          <div className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl px-2 md:px-3 md:py-1 tracking-tighter">{subHeading}</div>
+          <div className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl px-2 md:px-3 md:py-1 tracking-tighter">{subHeading}&nbsp;<span>_</span></div>
         </div>
         <div className="h-full p-4 border-b flex items-center justify-center">
           <div className="grid grid-cols-2 min-[425px]:grid-cols-3 gap-2 sm:grid-cols-4 lg:grid-cols-5">
