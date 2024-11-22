a096f6f97b2c4d3b9e5e7f53d0fb94e8663e5f30
nobleknightt <60701544+nobleknightt@users.noreply.github.com>
2024-11-22 19:19:16 +0530

    some formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center font-['Bricolage_Grotesque']">
+    <div className="w-screen h-screen flex items-center justify-center p-4 font-['Bricolage_Grotesque']">
       Nice to see you here! Hope you're having a great day!
     </div>
   )
