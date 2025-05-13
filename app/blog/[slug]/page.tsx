c73db2c37e45a6a499e1bc8cd14b4260169ee72f
Ajay Dandge <60701544+nobleknightt@users.noreply.github.com>
2025-05-13 18:32:39 +0530

    removed balanced wrapping from title and width from date

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -89,7 +89,7 @@ export default async function Blog({ params }) {
           }),
         }}
       />
-      <h1 className="title font-semibold text-2xl tracking-tighter">
+      <h1 className="font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
